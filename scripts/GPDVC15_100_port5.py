9e5a2fdc8ec093014c25fc01ba069769d3a83c72
tsutsumi-toshihisa <104.toshi.223@gmail.com>
2019-07-18 13:44:02 +0900

    launch_add

--- scripts/GPDVC15_100_port5.py
+++ scripts/GPDVC15_100_port5.py
@@ -10,7 +10,7 @@ from std_msgs.msg import Float64
 from std_msgs.msg import String
 from std_msgs.msg import Int32
 
-class GPDVC15_100_port5(object):
+class GPDVC15_100(object):
 
     def __init__(self):
 
