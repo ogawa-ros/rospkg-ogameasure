f187481d1e755f5e02e798b9dc348e8b1466ef28
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-08-21 01:47:52 +0900

    add

--- scripts/l218.py
+++ scripts/l218.py
@@ -27,7 +27,7 @@ class l218(object):
             for i in range(ch_num):
                 temp = list(self.l218.kelvin_reading_query(ch=0))
                 self.pub_list[i].publish(temp[i])
-                time.sleep(1)
+                time.sleep(2)
             continue
 
     def start_thread(self):
