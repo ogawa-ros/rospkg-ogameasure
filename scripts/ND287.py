147efaf1449cdebfd6fe340bbc89c5f6cdcbf243
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-08-28 16:37:41 +0900

    add

--- scripts/ND287.py
+++ scripts/ND287.py
@@ -54,6 +54,6 @@ if __name__ == '__main__':
     encorder = ND287()
     encorder.set_display()
     while not rospy.is_shutdown():
-        ret = ND287.get_az()
+        ret = encorder.get_az()
         pub_az.publish(float(az))
         continue
