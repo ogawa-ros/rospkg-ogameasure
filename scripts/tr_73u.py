d707f05eac215e2b6d287da281908d749cdde638
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2020-01-10 16:52:19 +0900

    add TandD

--- scripts/tr_73u.py
+++ scripts/tr_73u.py
@@ -23,13 +23,9 @@ class tr_73u(object):
     def publish_data(self):
         while not rospy.is_shutdown():
             data = self.ondotori.output_current_data()
-            d = struct.unpack('26B', data)
-            temp = (d[6]*16**2+d[5]-1000)/10
-            humid = (d[8]*16**2+d[7]-1000)/10
-            press = (d[10]*16**2+d[9])/10
-            self.pub_temp.publish(temp)
-            self.pub_humid.publish(humid)
-            self.pub_press.publish(press)
+            self.pub_temp.publish(data[0])
+            self.pub_humid.publish(data[1])
+            self.pub_press.publish(data[2])
             time.sleep(5)
         return
 
