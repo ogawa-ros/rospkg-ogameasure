881df0805ad61e335c8a50811ace1b6ba2425fe6
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2020-01-17 23:35:18 +0900

    add

--- scripts/fsw0010.py
+++ scripts/fsw0010.py
@@ -6,6 +6,7 @@ import time
 import sys
 import ogameasure
 import rospy
+import threading
 from std_msgs.msg import Float64
 from std_msgs.msg import String
 from std_msgs.msg import Int32
@@ -15,28 +16,63 @@ class fsw0010(object):
         host = rospy.get_param("~host")
         port = rospy.get_param("~port")
         com = ogameasure.ethernet(host, port)
-        self.sg = ogameasure.Phasematrix.FSW0010(com)
+        self.sg = ogameasure.Phasematrix.fsw0010(com)
 
 
         rospy.Subscriber("/dev/fsw0010/__IP__/f_cmd", Float64, self.set_freq)
         rospy.Subscriber("/dev/fsw0010/__IP__/power_cmd", Float64, self.set_power)
         rospy.Subscriber("/dev/fsw0010/__IP__/onoff_cmd", String, self.set_onoff)
 
+        self.freq_pub = rospy.Publisher("/dev/fsw0010/__IP__/freq",Float64,queue_size=1)
+
+        self.flag = True
+
     def set_freq(self,q):
+        self.flag = False
         self.sg.freq_set(q.data)
+        time.sleep(1)
+        self.flag = True
         return
 
     def set_power(self,q):
+        self.flag = False
         self.sg.power_set(q.data)
+        time.sleep(1)
+        self.flag = True
         return
 
     def set_onoff(self,q):
+        self.flag = False
         self.sg.output_set(q.data)
+        time.sleep(1)
+        self.flag = True
         return
 
+    def query_freq(self):
+        while not rospy.is_shutdown():
+            if self.flag == False:
+                time.sleep(0.1)
+                continue
+            elif self.flag == True:
+                try:
+                    f = self.sg.freq_query()
+                    self.freq_pub.publish(float(f))
+                    time.sleep(3)
+                except:
+                    pass
+                continue
+            else:
+                pass
+            continue
+
+    def start_thread(self):
+        th = threading.Thread(target = self.query_freq)
+        th.setDaemon(True)
+        th.start()
 
 
 if __name__ == '__main__':
     rospy.init_node(name)
     sg = fsw0010()
+    sg.start_thread()
     rospy.spin()
