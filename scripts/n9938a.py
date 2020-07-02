7088dd4c294bb356dde6fedfed7b09650e80b4c4
小西亜侑 <ayu@konishiayuunoMacBook-Pro.local>
2020-07-03 00:37:16 +0900

    edit

--- scripts/n9938a.py
+++ scripts/n9938a.py
@@ -9,6 +9,7 @@ import rospy
 import threading
 from std_msgs.msg import Bool
 from std_msgs.msg import Float64
+from std_msgs.msg import Int32
 from std_msgs.msg  import Float64MultiArray
 
 class n9938a(object):
@@ -31,7 +32,7 @@ class n9938a(object):
         rospy.Subscriber("/dev/n9938a/__IP__/vbw_set_cmd", Float64, self.vid_bw_set)
         rospy.Subscriber("/dev/n9938a/__IP__/rbw_query_cmd", Float64, self.vid_bw_query)
 
-        rospy.Subscriber("/dev/n9938a/__IP__/rbw_auto_cmd", Bool, self.resol_bw_auto_set)
+        rospy.Subscriber("/dev/n9938a/__IP__/rbw_auto_cmd", Int32, self.resol_bw_auto_set)
 
         self.flag = True
 
