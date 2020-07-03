ad0d784cf1e12baebf50f6fc10e25fbdc63a6418
小西亜侑 <ayu@konishiayuunoMacBook-Pro.local>
2020-07-03 10:08:56 +0900

    bugfix

--- scripts/n9938a.py
+++ scripts/n9938a.py
@@ -20,17 +20,19 @@ class n9938a(object):
         com = ogameasure.ethernet(host,port)
         self.sa = ogameasure.Keysight.N9938A(com)
 
-        self.pub = rospy.Publisher("/dev/n9938a/__IP__/spec",Float64MultiArray,queue_size=1)
+        self.pub1 = rospy.Publisher("/dev/n9938a/__IP__/spec",Float64MultiArray,queue_size=1)
 
         rospy.Subscriber("/dev/n9938a/__IP__/freq_start_cmd", Float64, self.start_freq_set)
         rospy.Subscriber("/dev/n9938a/__IP__/freq_stop_cmd", Float64, self.stop_freq_set)
         rospy.Subscriber("/dev/n9938a/__IP__/freq_center_cmd", Float64, self.center_freq_set)
 
         rospy.Subscriber("/dev/n9938a/__IP__/rbw_set_cmd", Float64, self.resol_bw_set)
-        rospy.Subscriber("/dev/n9938a/__IP__/rbw_query_cmd", Float64, self.resol_bw_query)
+        self.pub2 = rospy.Publisher("/dev/n9938a/__IP__/rbw_query_cmd",Float64MultiArray,queue_size=1)
+        #rospy.Subscriber("/dev/n9938a/__IP__/rbw_query_cmd", Float64, self.resol_bw_query)
 
         rospy.Subscriber("/dev/n9938a/__IP__/vbw_set_cmd", Float64, self.vid_bw_set)
-        rospy.Subscriber("/dev/n9938a/__IP__/rbw_query_cmd", Float64, self.vid_bw_query)
+        self.pub3 = rospy.Publisher("/dev/n9938a/__IP__/vbw_query_cmd",Float64MultiArray,queue_size=1)
+        #rospy.Subscriber("/dev/n9938a/__IP__/vbw_query_cmd", Float64, self.vid_bw_query)
 
         rospy.Subscriber("/dev/n9938a/__IP__/rbw_auto_cmd", Int32, self.resol_bw_auto_set)
 
@@ -43,7 +45,7 @@ class n9938a(object):
         while not rospy.is_shutdown():
             if self.flag == True:
                 spec = Float64MultiArray(data=self.sa.trace_data_query())
-                self.pub.publish(spec)
+                self.pub1.publish(spec)
             else:
                 pass
             time.sleep(0.1)
@@ -83,7 +85,7 @@ class n9938a(object):
         self.flag = True
         return
 
-
+        """
     def resol_bw_query(self):
         self.flag = False
         time.sleep(0.3)
@@ -91,6 +93,16 @@ class n9938a(object):
         time.sleep(0.1)
         self.flag = True
         return ret
+        """
+    def resol_bw_query(self):
+
+        self.flag = False
+        time.sleep(0.3)
+        ret = self.sa.resolution_bw_query()
+        self.pub2.publish(ret)
+        time.sleep(0.1)
+        self.flag = True
+        return
 
 
     def vid_bw_set(self,vbw):
@@ -101,7 +113,7 @@ class n9938a(object):
         self.flag = True
         return
 
-
+        """
     def vid_bw_query(self):
         self.flag = False
         time.sleep(0.3)
@@ -109,6 +121,18 @@ class n9938a(object):
         time.sleep(0.1)
         self.flag = True
         return ret
+        """
+
+    def vid_bw_query(self):
+
+        self.flag = False
+        time.sleep(0.3)
+        ret = self.sa.video_bw_query()
+        self.pub3.publish(ret)
+        time.sleep(0.1)
+        self.flag = True
+        return
+
 
 
     def resol_bw_auto_set(self,auto):
