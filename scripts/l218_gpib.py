845db99f7ca2171c989343de6d33cde8b89fc4b1
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2020-07-29 14:26:14 +0900

    add

--- scripts/l218_gpib.py
+++ scripts/l218_gpib.py
@@ -13,21 +13,32 @@ from std_msgs.msg import Int32
 
 class l218(object):
     def __init__(self):
-        host = rospy.get_param("~host")
-        gpibport = rospy.get_param("~gpibport")
-        com = ogameasure.gpib_prologix(host, gpibport)
+        self.host = rospy.get_param("~host")
+        self.gpibport = rospy.get_param("~gpibport")
+        self.com = ogameasure.gpib_prologix(host, gpibport)
         print(host)
         print(gpibport)
         self.l218 = ogameasure.Lakeshore.model218(com)
 
         self.pub_list = [rospy.Publisher("/dev/218/__IP__/temp/ch{0}".format(ch), Float64, queue_size=1) for ch in range(1,ch_num+1)]
 
+    def connect(self):
+        self.com = ogameasure.gpib_prologix(self.host, self.gpibport)
+        self.l218 = ogameasure.Lakeshore.model218(com)
+
     def temp_publisher(self,ch=0):
         while not rospy.is_shutdown():
-            for i in range(ch_num):
-                temp = list(self.l218.kelvin_reading_query(ch=0))
-                self.pub_list[i].publish(temp[i])
-                time.sleep(2)
+            try:
+                for i in range(ch_num):
+                    temp = list(self.l218.kelvin_reading_query(ch=0))
+                    self.pub_list[i].publish(temp[i])
+                    time.sleep(2)
+
+            except SocketError as e:
+                if e.errno != errno.ECONNRESET:
+                    time.sleep(60)
+                    self.connect()
+                pass # Handle error here.
             continue
 
     def start_thread(self):
